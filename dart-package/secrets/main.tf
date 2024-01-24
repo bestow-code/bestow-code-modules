eaba2dcaf05d4fb649fdd8a4a7365bd54288c31b
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-01-24 00:20:46 -0600

    wip

--- dart-package/secrets/main.tf
+++ dart-package/secrets/main.tf
@@ -3,13 +3,13 @@ data "github_repository" "repository" {
 }
 
 resource "github_actions_secret" "onepub_token" {
-  repository       = data.github_repository.repository.repo_id
+  repository       = data.github_repository.repository.full_name
   secret_name      = "onepub_token"
   plaintext_value  = var.onepub_token
 }
 
 resource "github_actions_secret" "dependabot_token" {
-  repository       = data.github_repository.repository.repo_id
+  repository       = data.github_repository.repository.full_name
   secret_name      = "dependabot_token"
   plaintext_value  = var.dependabot_token
 }
